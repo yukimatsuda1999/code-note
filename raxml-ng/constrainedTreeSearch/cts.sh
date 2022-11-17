95af6a4db094b08939dfedece587a3c7790ef2d1
yukimatsuda1999 <yuki.matsuda.r7@dc.tohoku.ac.jp>
2022-11-17 19:56:47 +0900

    add descriptions

--- raxml-ng/constrainedTreeSearch/cts.sh
+++ raxml-ng/constrainedTreeSearch/cts.sh
@@ -1,2 +1,6 @@
 #! /bin/zsh/
+
 raxml-ng --msa Obscn.fa --evaluate --tree Constrained.nwk --model GTR+G --threads 2
+
+# --evaluate	compute the likelihood of a given fixed tree topology by just optimizing model and/or branch length parameters on that fixed tree
+# --tree	tree topology
