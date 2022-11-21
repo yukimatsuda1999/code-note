46bee1c5584818bfbb1cb0118d071bca89ac121b
yukimatsuda1999 <yuki.matsuda.r7@dc.tohoku.ac.jp>
2022-11-21 14:37:36 +0900

    modified script

--- GSEA/gsea.R
+++ GSEA/gsea.R
@@ -1,6 +1,7 @@
 if (!require("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-
 BiocManager::install("fgsea")
+
+library(tidyverse)
 library(fgsea)
 
 sample = read_tsv("topGO/sample.tsv")
@@ -13,11 +14,11 @@ kegg_pathways = gmtPathways("GSEA/c2.cp.kegg.v2022.1.Hs.symbols.gmt")
 fgseaRes <- fgsea(
   pathways = kegg_pathways, 
   stats    = named_scores,
-  minSize  = 1,  # 15
+  minSize  = 15,  # 15
   maxSize  = length(named_scores)-1,  # 500
   scoreType = "pos"  # c("std", "pos", "neg")
-  )  
+  )
 
-head(fgseaRes[order(pval), ])
+head(fgseaRes %>% arrange(pval))
 
 
